e23416b0e079bf32602f9f09566c68fbce64a182
Aditi Shukla <121290452+Aditishukla29@users.noreply.github.com>
2023-09-04 16:21:18 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,24 @@
+//arrsay by literal
+var employee=["Aditi","Ravi","Aarav","Shukla"];
+    console.log( "Array by literal is : ");
+    for(value of employee){
+    console.log(value);
+}
+// array using new keyword
+var fruits=new Array();
+fruits[0]="apple";
+fruits[1]="mango";
+fruits[2]="banana";
+fruits[3]="orange";
+console.log("Array by using new keyword is : ")
+for(value of fruits){
+console.log(value);
+}
+
+// by Array Constructor {capital first letter}
+var Cars= new Array("love","you","aditi");
+console.log("array by constructor is : ")
+for( value of Cars){
+    console.log(value);
+}
+// ARRAY METHODS//
